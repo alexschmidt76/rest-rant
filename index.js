b6d5a258c064af7d43b0763c5e717ec6b93be180
Alex Schmidt <alex.schmidt767@gmail.com>
2022-09-29 19:22:52 -0500

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// DEPENDENCIES
+//Modules and Globas
 const express = require('express');
-
-// CONFIGURATION
 require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
 
-// MIDDLEWARE
+// Express Settings
+app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+app.use(express.static('public'));
 
-// Places
+// Controllers and Routs
+// places controller
 app.use('/places', require('./controllers/places'));
 
-// ROUTES
 // home page
 app.get('/', (req, res) => {
     res.render('home');
@@ -24,7 +24,7 @@ app.get('*', (req, res) => {
     res.status(404).render('error404');
 });
 
-// LISTEN
+// Listen for Connections
 app.listen(PORT, () => {
-    console.log('connected');
+    console.log(`listening on port ${PORT}`);
 });
