5b0c9d908badc6d83c7bb371243c983eefa80e27
Alex Schmidt <alex.schmidt767@gmail.com>
2022-10-01 22:44:52 -0500

    Delete functionality implemented

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,12 +1,19 @@
 const React = require('react');
 const Def = require('../default');
 
-function show (place) {
+function show(data) {
     return (
         <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
+            <main>
+                <h1>{data.place.name}</h1>
+                <p className='text-center'>
+                    {data.place.cuisines}
+                </p>
+                <img src={data.place.pic} alt={data.place.name}/>
+                <p className='text-center'>
+                    Located in {data.place.city}, {data.place.state}
+                </p>
+            </main>
         </Def>
     );
 }
