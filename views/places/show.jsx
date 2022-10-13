08569ff36ee67143d471d2b6ac4d9ec73b85d35e
Alex Schmidt <alex.schmidt767@gmail.com>
2022-10-13 18:53:08 -0500

    average rating now shows as a count of *'s

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,7 +7,25 @@ function show(data) {
             No comments yet!
         </h3>
     );
+    let rating = (
+        <h3 className='inactive'>
+            Not yet rated
+        </h3>
+    )
     if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce( (tot, c) => {
+            return tot + c.stars;
+        }, 0);
+        let averageRating = Math.round(sumRatings / data.place.comments.length);
+        let stars = '';
+        for (let i = 0; i < averageRating; i++) {
+            stars += '*';
+        }
+        rating = (
+            <h3>
+                {stars}
+            </h3>
+        )
         comments = data.place.comments.map( c => {
             return (
                 <div className='border' key={c.id}>
@@ -31,7 +49,7 @@ function show(data) {
                     <div className='col-sm-4'>
                         <div id='place-rating'>
                             <h2>Rating</h2>
-                            <p>Not yet rated.</p>
+                            {rating}
                         </div>
                         <div id='place-description'>
                             <h2>Description</h2>
